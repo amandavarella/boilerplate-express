04e6c70d493020d9e126996d3b4e5f4f544eda0d
Amanda Varella <amanda.varella@99designs.com>
2022-06-30 16:09:01 +1000

    Hello express

--- myApp.js
+++ myApp.js
@@ -4,11 +4,9 @@ let app = express();
 console.log ("Hello Worlda");
 
 
-function handler(req, res) {
-    res.send('Hello Express');
-}
-
-app.get("/",handler);
+app.get("/", function(req, res) {
+    res.send("Hello Express");
+  });
   
 
 
