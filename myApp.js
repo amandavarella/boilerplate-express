56096a208b6db67befce4f819410844929ecf192
Amanda Varella <amanda.varella@99designs.com>
2022-07-04 08:12:35 +1000

    Serving a json file

--- myApp.js
+++ myApp.js
@@ -8,9 +8,12 @@ console.log(__dirname);
 app.use("/public", express.static(__dirname + "/public"));
 
 app.get("/", function(req, res) {
-res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", function(req, res){
+    res.json({"message":"Hello json"});
+});
 
 
 
