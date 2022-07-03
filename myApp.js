5a38f8e9e4b9af35d368199f35139879e71b247d
Amanda Varella <amanda.varella@99designs.com>
2022-07-04 08:41:06 +1000

    Adding env file

--- myApp.js
+++ myApp.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
 
@@ -11,8 +13,12 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
 
+
 app.get("/json", function(req, res){
-    res.json({"message":"Hello json"});
+    if(process.env.MESSAGE_STYLE=="uppercase")
+        res.json({"message":"HELLO JSON"});
+    else
+        res.json({"message":"Hello json"});
 });
 
 
